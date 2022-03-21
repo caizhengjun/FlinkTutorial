2471578025a3a31864a7b138a0417b3940b4e549
caizhengjun <caizhengjun01@gmail.com>
2022-03-21 22:53:11 +0800

    wordCount

---
+++ src/main/java/com/iflytek/wc/BatchWordCount.java
@@ -0,0 +1,22 @@
+package com.iflytek.wc;
+
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.operators.AggregateOperator;
+import org.apache.flink.api.java.operators.DataSource;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+public class BatchWordCount {
+    public static void main(String[] args) throws Exception {
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSource<String> lineDataSource = env.readTextFile("input/word_flink.txt");
+        AggregateOperator<Tuple2<String, Long>> sum = lineDataSource.flatMap((String line, Collector<Tuple2<String, Long>> out) -> {
+            String[] words = line.split(" ");
+            for (String word : words) {
+                out.collect(Tuple2.of(word, 1L));
+            }
+        }).returns(Types.TUPLE(Types.STRING, Types.LONG)).groupBy(0).sum(1);
+        sum.print();
+    }
+}
